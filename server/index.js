8e833d886c14406be7db8c9d354e6be3e69d44de
Sebastian Brear <ssbrear@ucdavis.edu>
2021-04-15 15:57:37 -0700

    set up for dev as well as prod

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/api", async (req, res) => {
+  console.log("ping");
   const backendPosts = await sequelize.models.Post.findAll();
   const postData = [];
   backendPosts.forEach((x) => {
